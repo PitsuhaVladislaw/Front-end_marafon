2caeec53453b9000bd688f7dc310f4531a6faaa0
Vladislaw <vladikpizza02@gmail.com>
2022-11-09 00:20:51 +0300

    feat: add new function for every part

--- task-1/script.js
+++ task-1/script.js
@@ -1,15 +1,21 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlaggin(activeSlide) {
+    const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-    slide.addEventListener('click', () =>  {
-        clearActiveClasses()
+    slides[activeSlide].classList.add("active")
 
-        slide.classList.add('active')
-    })
+    for (const slide of slides) {
+        slide.addEventListener('click', () =>  {
+            clearActiveClasses()
+
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidesPlaggin(4);
