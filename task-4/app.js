af54a9267de600bf06cccee444d16a1b16582fbc
Vladislaw <vladikpizza02@gmail.com>
2022-11-11 23:50:19 +0300

    feat: add new parts for js-files in task-3 && -4

--- task-4/app.js
+++ task-4/app.js
@@ -15,6 +15,33 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
     board.append(square);
 }
 
+/*
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => setColor);
+
+    square.addEventListener('mouseleave', () => removeColor);
+
+    board.append(square);
+}
+
+function setColor(event) {
+    const element = event.target;
+    const color = getRandomColor();
+    
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10 ${color}`;
+}
+
+function removeColor(event) {
+    const element = event.target;
+    element.style.backgroundColor = '#1d1d1d';
+    element.style.boxShadow = '0 0 2px #000';
+}
+*/
+
 function setColor(elem) {
     const color = getRandomColor();
     elem.style.backgroundColor = color;
@@ -31,3 +58,9 @@ function getRandomColor() {
 
     return colors[index];
 }
+
+/*
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+*/
